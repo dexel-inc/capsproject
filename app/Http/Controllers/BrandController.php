925d69f894a620b6969387aa9ace46a0349d39c5
Anderson <cardonaac389@gmail.com>
2026-02-05 10:36:12 -0500

    feature: add store method

--- app/Http/Controllers/BrandController.php
+++ app/Http/Controllers/BrandController.php
@@ -2,20 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Actions\StoreBrandAction;
+use App\Http\Requests\StoreBrandRequest;
 use App\Models\Brand;
 use Inertia\Inertia;
 
 class BrandController extends Controller
 {
-    public function index()
+    public function index(): \Inertia\Response
     {
         $brands = Brand::all();
 
         return Inertia::render('brands/Index', ['brands' => $brands]);
     }
 
-    public function store()
+    public function store(StoreBrandRequest $request, StoreBrandAction $action): void
     {
-
+        $action->execute($request->validated());
     }
 }
