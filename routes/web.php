889091d51f5ae3e20220a9b1ba7253824bfc01b9
Anderson <cardonaac389@gmail.com>
2026-02-08 17:51:04 -0500

    feature: add products route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\BrandController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
@@ -9,3 +10,4 @@ Route::get('/', function () {
 })->name('home');
 
 Route::resource('brands', BrandController::class);
+Route::resource('products', ProductController::class);
