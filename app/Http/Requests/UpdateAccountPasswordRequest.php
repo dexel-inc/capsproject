beb11d07f0b88bdf88ea7273898ca592601bd342
Anderson <cardonaac389@gmail.com>
2026-02-28 20:47:24 -0500

    feat(account): update UpdateAccountPasswordRequest.php

---
+++ app/Http/Requests/UpdateAccountPasswordRequest.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rules\Password;
+
+final class UpdateAccountPasswordRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return $this->user() !== null;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'current_password' => ['required', 'current_password'],
+            'password' => ['required', 'string', Password::defaults(), 'confirmed'],
+        ];
+    }
+}
+
