f24c4f3c1064d1c88d03f6bf5e4bfcf7f0c2264b
Anderson <cardonaac389@gmail.com>
2026-02-28 20:47:26 -0500

    feat(home): update index.ts

--- resources/js/components/sections/home/index.ts
+++ resources/js/components/sections/home/index.ts
@@ -2,4 +2,5 @@ export { default as CatalogSection } from './CatalogSection';
 export { default as HeroSection } from './HeroSection';
 export { default as InfiniteMarquee } from './InfiniteMarquee';
 export { default as FeaturedProductsCarousel } from './FeaturedProductsCarousel';
+export { default as FaqPreviewSection } from './FaqPreviewSection';
 export { default as MinimalFooter } from './MinimalFooter';
